525dee8da5362db63e9b4346e3a18b0a863a9452
ahmed_khaled_1 <ahmedkhaledmohamed9@gmail.com>
2021-08-16 05:20:04 +0200

    Operators precedence exercise

--- queries.sql
+++ queries.sql
@@ -106,6 +106,27 @@ for rangs
 -- operators precedence: multiple operators is evaluated based on the priority of operators 
 -- parenthese > multiplicity/division > subtraction/addition > NOT > AND > OR 
 
+-- EXERCISE: 
+/*
+* DB: Store
+* Table: Customers
+* Question: 
+* Select people either under 30 or over 50 with an income above 50000
+* Include people that are 50
+* that are from either Japan or Australia
+*/
+SELECT firstname, income, age from customers
+where  (income >50000 and (age >30 or age >=50))  and (country = 'Japan' OR country = 'Australia');
+
+/*
+* DB: Store
+* Table: Orders
+* Question: 
+* What was our total sales in June of 2004 for orders over 100 dollars?
+*/
+SELECT SUM(totalamount) from orders
+WHERE (orderdate >= '2004-06-01' AND orderdate <= '2004-06-30') 
+AND totalamount > 100
 
 
 
