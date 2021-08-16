0ed7a7396df561006cec620fdcb0d559341277fe
ahmed_khaled_1 <ahmedkhaledmohamed9@gmail.com>
2021-08-16 14:30:54 +0200

    THREE-VALUED LOGIC

--- queries.sql
+++ queries.sql
@@ -174,3 +174,49 @@ SELECT sum(coalase(age, 15)) AS age
 FROM students
 -- here if there a null field then put the age to 15, 
 -- so that to not leave a person that should have an age to reach approx sum all the students
+
+
+-- THREE-VALUED LOGIC
+-- EXPRESSION COULD RETURN TRUE OR NULL OR FALSE 
+-- sidenot: NULL=NULL ,NULL<>NULL both return null not true 
+-- (NULL=1)OR(1=1) return true
+-- (NULL=1) AND (0=1) return null because null and anything is nullable
+
+SELECT <column> 
+FROM <table> 
+WHERE <column> IS NULL  
+-- return true if it's nullable. IS is asking is there any NULLABLE or not
+
+SELECT <column> 
+FROM <table> 
+WHERE <column> = NULL 
+-- return null if there is nullable. because null= null return null 
+
+SELECT <column> 
+FROM <table> 
+WHERE (<column> = NULL) OR (NOT <column> = NULL) 
+-- return nothing, all row are going to be rejected, null equality return nothing   
+
+SELECT <column> 
+FROM <table> 
+WHERE (<column> IS NULL) OR ( <column> IS NOT NULL)
+-- return everything. rows that have value and rows that don't have value
+
+
+--THREE-VALUED EXERCISE
+
+/*
+* DB: Store
+* Table: customers
+* Question: adjust the following query to display the null values as "No Address"
+*/
+SELECT COALESCE(address2, 'No Address') AS address
+FROM customers
+
+
+
+
+
+
+
+ 
