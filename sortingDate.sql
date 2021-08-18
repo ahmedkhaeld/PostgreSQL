2e0c5a8f205bc4dbd70838569818ad31ff3c5c2e
ahmed_khaled1 <ahmedkhaledmohamed9@gmail.com>
2021-08-18 19:18:59 +0200

    sorting

---
+++ sortingDate.sql
@@ -0,0 +1,10 @@
+--sorting data: ASC is the default sort
+Select * from customers
+Order by columns asc/desc
+
+Select * from customers
+Order by name asc
+
+Select  firstname, lastname from customers
+Order by firstname asc, lastname desc
+
